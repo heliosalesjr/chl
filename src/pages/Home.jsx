91b91c17ee020326eb7c839f5703acabfb8e36bf
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-04-17 12:44:23 -0300

    main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,7 @@
 const Home = () => (
     <div>
-      <h1 className="text-3xl font-bold mb-4">📘 Custom Hooks Playground</h1>
-      <p className="text-lg text-gray-700">
-        Bem-vindo, Hélio! Explore os exemplos práticos de custom hooks usando as
-        opções no menu acima. 💡
-      </p>
+      <h1 className="text-3xl font-bold mb-4 text-center align-middle content-center">📘 Custom Hooks Playground</h1>
+      
     </div>
   );
   
