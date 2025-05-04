cae051519cf4b1b5b09986563e6a70c9ebf787a5
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-05-04 20:44:12 -0300

    text hover effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 const Home = () => (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-center">📘 Custom Hooks Playground</h1>
-      
-    </div>
+  <div className="h-[90vh] flex items-center justify-center">
+    <h1 className="text-3xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 transition-all duration-500 hover:scale-110 hover:bg-gradient-to-r hover:from-pink-500 hover:via-yellow-500 hover:to-green-500">
+      📘 Custom Hooks Playground
+    </h1>
+  </div>
   );
   
   export default Home;
